d5ea2501e4dc7bd3393555c7bc4a2542aaf1f616
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-01-11 21:27:51 +0100

    GUI for health and wave info

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using ScriptableObjects;
+using TMPro;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
 	[SerializeField] private Wave[] waves;
 
-    private void Start()
-    {
-	   	StartCoroutine(SpawnWaves());
-    }
+	[SerializeField] private TextMeshProUGUI waveText;
+	[SerializeField] private TextMeshProUGUI waveCountdownText;
 
-    private IEnumerator SpawnWaves()
-    {
-	    foreach (Wave wave in waves)
-	    {
-		    for (int i = 0; i < wave.delaySincePreviousWave; i++)
-		    {
-			    Debug.Log(wave.delaySincePreviousWave - i);
+	private void Start()
+	{
+		StartCoroutine(SpawnWaves());
+	}
+
+	private IEnumerator SpawnWaves()
+	{
+		for (int waveNr = 0; waveNr < waves.Length; waveNr++)
+		{
+			Wave wave = waves[waveNr];
+			waveText.text = "Current wave: " + waveNr;
+			for (int i = 0; i < wave.delaySincePreviousWave + 1; i++)
+			{
+				waveCountdownText.text = $"Time till next wave: {wave.delaySincePreviousWave - i}";
 				yield return new WaitForSeconds(1);
-		    }
-		    wave.StartWave();
-	    }
-    }
+			}
+
+			wave.StartWave();
+		}
+
+		//All waves have spawned, so clear the text
+		waveText.text = "No waves left";
+		waveCountdownText.text = "";
+	}
 }
